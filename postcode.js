1db4eca9a0885c945e27392a736c327ec14586a0
ozdemirs-hub <ozdemir.sarioglu@icloud.com>
2026-08-31 12:41:47 +1000

    Update postcode.js

--- postcode.js
+++ postcode.js
@@ -3,7 +3,35 @@ function testCXoneFields() {
     var postcode = document.getElementById('4');
     var product = document.getElementById('product_type');
 
-    if (postcode && product) {
-            postcode.value = 'FOUND BOTH';
+    if (!postcode || !product) {
+        return;
+    }
+
+    var utilities = null;
+
+    for (var i = 0; i < product.options.length; i++) {
+
+        if (product.options[i].value === 'Utilities') {
+            utilities = product.options[i];
+            break;
+        }
+    }
+
+    if (!utilities) {
+        return;
+    }
+
+    if (postcode.value.trim() === '1234' || postcode.value.trim() === '4321') {
+
+        utilities.disabled = true;
+
+        if (product.value === 'Utilities') {
+            product.value = '';
+        }
+
+    } else {
+
+        utilities.disabled = false;
+
     }
 }
