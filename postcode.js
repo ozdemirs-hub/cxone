ed95410d4f925fee575d560130732329c7f849e3
ozdemirs-hub <ozdemir.sarioglu@icloud.com>
2026-08-31 10:54:45 +1000

    Update postcode.js

--- postcode.js
+++ postcode.js
@@ -1,14 +1,64 @@
-function checkPostcode() {
-    var postcode = document.getElementById('4').value.trim();
-    var productType = document.getElementById('product_type');
+(function () {
 
-    if (postcode === '1234' || postcode === '4321') {
-        productType.options[3].disabled = true;
+    function initialisePostcodeRestriction() {
 
-        if (productType.value === 'Utilities') {
-            productType.value = '';
+        var postcodeField = document.getElementById('4');
+        var productType = document.getElementById('product_type');
+
+        if (!postcodeField || !productType) {
+            return;
         }
-    } else {
-        productType.options[3].disabled = false;
+
+        function checkPostcode() {
+
+            var postcode = postcodeField.value.trim();
+            var utilitiesOption = null;
+
+            for (var i = 0; i < productType.options.length; i++) {
+
+                if (productType.options[i].value === 'Utilities') {
+                    utilitiesOption = productType.options[i];
+                    break;
+                }
+            }
+
+            if (!utilitiesOption) {
+                return;
+            }
+
+            if (postcode === '1234' || postcode === '4321') {
+
+                utilitiesOption.disabled = true;
+
+                if (productType.value === 'Utilities') {
+                    productType.value = '';
+                }
+
+            } else {
+
+                utilitiesOption.disabled = false;
+
+            }
+        }
+
+        postcodeField.addEventListener('input', checkPostcode);
+        postcodeField.addEventListener('change', checkPostcode);
+
+        checkPostcode();
     }
-}
+
+
+    if (document.readyState === 'loading') {
+
+        document.addEventListener(
+            'DOMContentLoaded',
+            initialisePostcodeRestriction
+        );
+
+    } else {
+
+        initialisePostcodeRestriction();
+
+    }
+
+})();
