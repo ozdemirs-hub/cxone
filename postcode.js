5c443d596465bbb528c765d6095b7944ebb44b9f
ozdemirs-hub <ozdemir.sarioglu@icloud.com>
2026-08-31 14:11:20 +1000

    Update postcode.js

--- postcode.js
+++ postcode.js
@@ -120,7 +120,7 @@
 
     function loadPostcodes() {
 
-        fetch('https://YOUR-USERNAME.github.io/YOUR-REPOSITORY/utilities-postcodes.json')
+        fetch('https://ozdemirs-hub.github.io/cxone/utilities-postcodes.json')
 
             .then(function (response) {
 
