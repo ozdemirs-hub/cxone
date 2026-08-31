8c1b4354e5ec44823117cbafb60d16402c8462b4
ozdemirs-hub <ozdemir.sarioglu@icloud.com>
2026-08-31 11:14:43 +1000

    Update postcode.js

--- postcode.js
+++ postcode.js
@@ -1,64 +1,8 @@
 (function () {
-
-    function initialisePostcodeRestriction() {
-
-        var postcodeField = document.getElementById('4');
-        var productType = document.getElementById('product_type');
-
-        if (!postcodeField || !productType) {
-            return;
+    
+        var postcode = document.getElementById('4');
+        if (postcode) {
+            postcode.value = 'script loaded';
         }
-
-        function checkPostcode() {
-
-            var postcode = postcodeField.value.trim();
-            var utilitiesOption = null;
-
-            for (var i = 0; i < productType.options.length; i++) {
-
-                if (productType.options[i].value === 'Utilities') {
-                    utilitiesOption = productType.options[i];
-                    break;
-                }
-            }
-
-            if (!utilitiesOption) {
-                return;
-            }
-
-            if (postcode === '1234' || postcode === '4321') {
-
-                utilitiesOption.disabled = true;
-
-                if (productType.value === 'Utilities') {
-                    productType.value = '';
-                }
-
-            } else {
-
-                utilitiesOption.disabled = false;
-
-            }
-        }
-
-        postcodeField.addEventListener('input', checkPostcode);
-        postcodeField.addEventListener('change', checkPostcode);
-
-        checkPostcode();
-    }
-
-
-    if (document.readyState === 'loading') {
-
-        document.addEventListener(
-            'DOMContentLoaded',
-            initialisePostcodeRestriction
-        );
-
-    } else {
-
-        initialisePostcodeRestriction();
-
-    }
 
 })();
