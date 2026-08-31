a75a5f2cf471b09ad2085bd3a8f792f77ea80e57
ozdemirs-hub <ozdemir.sarioglu@icloud.com>
2026-08-31 12:16:37 +1000

    Update postcode.js

--- postcode.js
+++ postcode.js
@@ -1,4 +1,75 @@
-function testButton() {
-    
-        var postcode = document.getElementById('4').value='SCRIPT WORKING';
-}
+(function () {
+
+    function checkPostcode() {
+
+        var postcodeField = document.getElementById('4');
+        var productType = document.getElementById('product_type');
+
+        if (!postcodeField || !productType) {
+            return;
+        }
+
+        var postcode = postcodeField.value.trim();
+
+        var utilitiesOption = null;
+
+        for (var i = 0; i < productType.options.length; i++) {
+
+            if (productType.options[i].value === 'Utilities') {
+                utilitiesOption = productType.options[i];
+                break;
+            }
+
+        }
+
+        if (!utilitiesOption) {
+            return;
+        }
+
+        if (postcode === '1234' || postcode === '4321') {
+
+            utilitiesOption.disabled = true;
+
+            if (productType.value === 'Utilities') {
+                productType.value = '';
+            }
+
+        } else {
+
+            utilitiesOption.disabled = false;
+
+        }
+
+    }
+
+
+    function initialisePostcodeRestriction() {
+
+        var postcodeField = document.getElementById('4');
+
+        if (!postcodeField) {
+            return;
+        }
+
+        postcodeField.addEventListener('input', checkPostcode);
+        postcodeField.addEventListener('change', checkPostcode);
+
+        checkPostcode();
+
+    }
+
+
+    if (document.readyState === 'loading') {
+
+        document.addEventListener(
+            'DOMContentLoaded',
+            initialisePostcodeRestriction
+        );
+
+    } else {
+
+        initialisePostcodeRestriction();
+
+    }
+
+})();
