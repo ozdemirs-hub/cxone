f1d572ebea0c010cca2902afd64e42d0e92a60c0
ozdemirs-hub <ozdemir.sarioglu@icloud.com>
2026-09-16 15:57:25 +1000

    Update postcode.js

--- postcode.js
+++ postcode.js
@@ -1,262 +1,559 @@
-// ============================================================
-// NICE CXone Custom Form
-// Dynamic Self-Contained Callback Calendar + Time
-// ============================================================
+(function () {
 
 
-// ============================================================
-// Stylesheet links
-// ============================================================
 
-html.append("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' integrity='sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==' crossorigin='anonymous'>")
-html.append("<link rel='stylesheet' href='//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>")
+    var utilitiesPostcodes = [];
 
 
-// ============================================================
-// JavaScript
-// ============================================================
 
-html.append("<script>")
+    var JSON_URL = 'https://ozdemirs-hub.github.io/cxone/utilities-postcodes.json';
 
 
-// ============================================================
-// Business field
-// ============================================================
 
-html.append("function toggleBusinessField(){{")
 
-html.append(" var accountType=document.getElementById('account_type').value;")
-html.append(" var group=document.getElementById('business_name_group');")
-html.append(" var field=document.getElementById('business_name');")
 
-html.append(" if(accountType==='Business'){{")
-html.append("  group.style.display='block';")
-html.append("  field.required=true;")
-html.append(" }}else{{")
-html.append("  group.style.display='none';")
-html.append("  field.required=false;")
-html.append("  field.value='';")
-html.append(" }}")
+    function getFields() {
 
-html.append("}")
 
 
-// ============================================================
-// End JavaScript
-// ============================================================
+        return {
 
-html.append("</script>")
+            state: document.getElementById('3'),
 
-// ============================================================
-// External JavaScript - Postcode restriction
-// ============================================================
+            postcode: document.getElementById('4'),
 
-html.append("<script type='text/javascript' src='https://ozdemirs-hub.github.io/cxone/postcode.js'></script>")
 
-// ============================================================
-// FORM CONTAINER
-// ============================================================
 
-html.append("<div class='container'>")
+            utilities: document.getElementById('product_utilities'),
 
+            ovc: document.getElementById('product_ovc'),
 
-// ============================================================
-// TOP MESSAGE
-// ============================================================
 
-html.append("<div class='alert alert-info' role='alert'>")
-html.append(" {wfdScript}")
-html.append("</div>")
 
+            callbackUtilitiesGroup:
 
-// ============================================================
-// FIRST NAME
-// ============================================================
+                document.getElementById('callback_datetime_utilities_group'),
 
-html.append("<div class='form-group'>")
 
-html.append(" <label class='control-label' style='font-weight:bold; color:red;'>First Name*:</label>")
 
-html.append(" <input type='text' class='form-control input-sm' id='1' name='scout_first_name' value='' required>")
+            callbackOvcGroup:
 
-html.append("</div>")
+                document.getElementById('callback_datetime_ovc_group')
 
+        };
 
-// ============================================================
-// LAST NAME
-// ============================================================
 
-html.append("<div class='form-group'>")
 
-html.append(" <label class='control-label' style='font-weight:bold; color:red;'>Surname*:</label>")
+    }
 
-html.append(" <input type='text' class='form-control input-sm' id='2' name='scout_last_name' value='' required>")
 
-html.append("</div>")
 
 
-// ============================================================
-// STATE
-// ============================================================
 
-html.append("<div class='form-group'>")
+    function postcodeIsApproved(postcode) {
 
-html.append(" <label class='control-label'>State:</label>")
 
-html.append(" <input type='text' class='form-control input-sm' id='3' name='scout_state' value=''>")
 
-html.append("</div>")
+        for (var i = 0; i < utilitiesPostcodes.length; i++) {
 
 
-// ============================================================
-// POSTCODE
-// ============================================================
 
-html.append("<div class='row'>")
+            if (String(utilitiesPostcodes[i]).trim() === postcode) {
 
-html.append("<div class='col-sm-6'>")
-html.append("<div class='form-group'>")
 
-html.append(" <label class='control-label'>Postcode:</label>")
 
-html.append(" <input type='text' class='form-control input-sm' id='4' name='scout_postcode' value=''>")
+                return true;
 
-html.append("</div>")
 
-html.append("</div>")
 
-html.append("</div>")
+            }
 
 
-// ============================================================
-// PHONE
-// ============================================================
 
-html.append("<div class='form-group'>")
+        }
 
-html.append(" <label class='control-label'>Phone:</label>")
 
-html.append(" <input type='text' class='form-control input-sm' id='5' name='scout_phone_number' value='{global:ANI}' readonly>")
 
-html.append("</div>")
+        return false;
 
 
-// ============================================================
-// EMAIL
-// ============================================================
 
-html.append("<div class='form-group'>")
+    }
 
-html.append(" <label class='control-label'>Email:</label>")
 
-html.append(" <input type='text' class='form-control input-sm' id='6' name='scout_email' value=''>")
 
-html.append("</div>")
 
 
-// ============================================================
-// TRANSFER ID
-// ============================================================
+    function updateUtilities() {
 
-html.append("<div class='form-group'>")
 
-html.append(" <label class='control-label'>Transfer ID:</label>")
 
-html.append(" <input type='text' class='form-control input-sm' name='transfer_id' value='{_ScoutTransferID}' readonly>")
+        var fields = getFields();
 
-html.append("</div>")
 
 
-// ============================================================
-// PRODUCT TYPE
-// ============================================================
+        if (!fields.state ||
 
-html.append("<div class='form-group'>")
+            !fields.postcode ||
 
-html.append(" <label class='control-label' style='font-weight:bold; color:red;'>Product Type*:</label>")
+            !fields.utilities) {
 
-html.append(" <div class='checkbox'>")
-html.append("  <label>")
-html.append("   <input type='checkbox' id='product_health' name='product_type' value='Health' checked>")
-html.append("   Health Sales")
-html.append("  </label>")
-html.append(" </div>")
 
-html.append(" <div class='checkbox'>")
-html.append("  <label>")
-html.append("   <input type='checkbox' id='product_ovc' name='product_type' value='OVC'>")
-html.append("   OVC")
-html.append("  </label>")
-html.append(" </div>")
 
-html.append(" <div class='checkbox'>")
-html.append("  <label>")
-html.append("   <input type='checkbox' id='product_utilities' name='product_type' value='Utilities'>")
-html.append("   Utilities")
-html.append("  </label>")
-html.append(" </div>")
+            return;
 
-html.append("</div>")
 
 
-// ============================================================
-// CALLBACK DATE & TIME Utilities
-// Hidden by default
-// ============================================================
+        }
 
-html.append("<div class='form-group' id='callback_datetime_utilities_group' style='display:none;'>")
-html.append(" <label class='control-label'>Callback Date Time Utilities:</label>")
-html.append(" <input type='datetime-local' class='form-control input-sm' id='callback_datetime_utilities' name='callback_datetime_utilities' value=''>")
-html.append("</div>")
 
-// ============================================================
-// CALLBACK DATE & TIME Utilities
-// Hidden by default
-// ============================================================
 
-html.append("<div class='form-group' id='callback_datetime_ovc_group' style='display:none;'>")
-html.append(" <label class='control-label'>Callback Date Time OVC:</label>")
-html.append(" <input type='datetime-local' class='form-control input-sm' id='callback_datetime_ovc' name='callback_datetime_ovc' value=''>")
-html.append("</div>")
 
 
+        var state = fields.state.value.trim().toUpperCase();
 
-// ============================================================
-// FLATPICKR
-// ============================================================
 
-// Flatpickr CSS
-html.append("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css'>")
 
-// Flatpickr library
-html.append("<script src='https://cdn.jsdelivr.net/npm/flatpickr'></script>")
+        var postcode = fields.postcode.value.trim();
 
-// Our GitHub JavaScript
-html.append("<script src='https://asset.compareclub.com.au/energy/reengagement/callback-picker.js'></script>")
 
 
-// ============================================================
-// NOTES
-// ============================================================
 
-html.append("<div class='form-group'>")
 
-html.append(" <label class='control-label'>Notes:</label>")
+        /*
 
-html.append(" <textarea class='form-control' id='7' name='scout_notes' rows='4' maxlength='1000' style='resize:vertical;'></textarea>")
+         * NSW and VIC are never allowed.
 
-html.append("</div>")
+         */
 
 
-// ============================================================
-// SUBMIT
-// ============================================================
 
-html.append("<div class='form-group'>")
+        if (state === 'NSW' || state === 'VIC') {
 
-html.append(" <input type='submit' class='btn btn-md btn-success' value='Create a Lead'>")
 
-html.append("</div>")
 
+            fields.utilities.disabled = true;
 
-html.append("</div>")
+            fields.utilities.checked = false;
+
+
+
+            updateCallback();
+
+
+
+            return;
+
+
+
+        }
+
+
+
+
+
+        /*
+
+         * Check postcode against JSON.
+
+         */
+
+
+
+        if (postcodeIsApproved(postcode)) {
+
+
+
+            fields.utilities.disabled = false;
+
+
+
+        } else {
+
+
+
+            fields.utilities.disabled = true;
+
+            fields.utilities.checked = false;
+
+
+
+        }
+
+
+
+
+
+        updateCallback();
+
+
+
+    }
+
+
+
+
+
+    function updateCallback() {
+
+
+
+        var fields = getFields();
+
+
+
+        if (!fields.callbackUtilitiesGroup ||
+
+            !fields.callbackOvcGroup) {
+
+
+
+            return;
+
+
+
+        }
+
+
+
+
+
+        /*
+
+         * OVC callback section
+
+         */
+
+
+
+        if (fields.ovc && fields.ovc.checked) {
+
+
+
+            fields.callbackOvcGroup.style.display = '';
+
+
+
+        } else {
+
+
+
+            fields.callbackOvcGroup.style.display = 'none';
+
+
+
+        }
+
+
+
+
+
+        /*
+
+         * Utilities callback section
+
+         */
+
+
+
+        if (fields.utilities && fields.utilities.checked) {
+
+
+
+            fields.callbackUtilitiesGroup.style.display = '';
+
+
+
+        } else {
+
+
+
+            fields.callbackUtilitiesGroup.style.display = 'none';
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+    function loadJSON() {
+
+
+
+        fetch(JSON_URL)
+
+
+
+            .then(function (response) {
+
+
+
+                if (!response.ok) {
+
+
+
+                    throw new Error(
+
+                        'HTTP error ' + response.status
+
+                    );
+
+
+
+                }
+
+
+
+                return response.json();
+
+
+
+            })
+
+
+
+            .then(function (data) {
+
+
+
+                utilitiesPostcodes = data;
+
+
+
+                updateUtilities();
+
+
+
+            })
+
+
+
+            .catch(function (error) {
+
+
+
+                console.log(
+
+                    'ERROR loading Utilities postcode JSON: ' +
+
+                    error.message
+
+                );
+
+
+
+            });
+
+
+
+    }
+
+
+
+
+
+    function initialise() {
+
+
+
+        var fields = getFields();
+
+
+
+
+
+        /*
+
+         * Wait for CXone form fields.
+
+         */
+
+
+
+        if (!fields.state ||
+
+            !fields.postcode ||
+
+            !fields.utilities ||
+
+            !fields.ovc ||
+
+            !fields.callbackUtilitiesGroup ||
+
+            !fields.callbackOvcGroup) {
+
+
+
+            setTimeout(initialise, 500);
+
+
+
+            return;
+
+
+
+        }
+
+
+
+
+
+        /*
+
+         * Initially hide BOTH callback sections.
+
+         */
+
+
+
+        updateCallback();
+
+
+
+
+
+        /*
+
+         * State changes.
+
+         */
+
+
+
+        fields.state.addEventListener('input', function () {
+
+
+
+            updateUtilities();
+
+
+
+        });
+
+
+
+
+
+        fields.state.addEventListener('change', function () {
+
+
+
+            updateUtilities();
+
+
+
+        });
+
+
+
+
+
+        /*
+
+         * Postcode changes.
+
+         */
+
+
+
+        fields.postcode.addEventListener('input', function () {
+
+
+
+            updateUtilities();
+
+
+
+        });
+
+
+
+
+
+        fields.postcode.addEventListener('change', function () {
+
+
+
+            updateUtilities();
+
+
+
+        });
+
+
+
+
+
+        /*
+
+         * OVC checkbox changes.
+
+         */
+
+
+
+        fields.ovc.addEventListener('change', function () {
+
+
+
+            updateCallback();
+
+
+
+        });
+
+
+
+
+
+        /*
+
+         * Utilities checkbox changes.
+
+         */
+
+
+
+        fields.utilities.addEventListener('change', function () {
+
+
+
+            updateCallback();
+
+
+
+        });
+
+
+
+
+
+        /*
+
+         * Load postcode list.
+
+         */
+
+
+
+        loadJSON();
+
+
+
+    }
+
+
+
+
+
+    setTimeout(initialise, 500);
+
+
+
+})();
